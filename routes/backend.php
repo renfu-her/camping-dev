b156c5bc89560b7fe6c74e7d5aa003b910e47a2c
Patrick Her <renfu.her@gmail.com>
2024-07-31 16:49:38 +0800

    Refactor backend routes to use ProductController for home page

--- routes/backend.php
+++ routes/backend.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Backend\CategoryController;
 
 Route::group(['prefix' => 'backend', 'as' => 'backend.'], function () {
-    // Route::get('/', [CategoryController::class, 'index'])->name('home');
+    Route::get('/', [ProductController::class, 'index'])->name('home');
     Route::resource('category', CategoryController::class);
     Route::resource('product', ProductController::class);
 });
